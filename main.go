05278cb560873d2f55de97bc5e58e37176973e5e
maziar <sojodyan@gmail.com>
2024-10-04 15:15:46 -0400

    add minimum post

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -13,8 +15,18 @@ type Message struct {
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Go API\n")
 }
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	body, _ := io.ReadAll(r.Body)
+	json.Unmarshal(body, &msg)
+	w.Header().Set("Content/Type", "application/json")
+	json.NewEncoder(w).Encode(msg)
+}
+
 func main() {
 	http.HandleFunc("/", getHandler)
+	http.HandleFunc("/post", postHandler)
+
 	pNumber := ":8091"
 	fmt.Printf("Server is running on the port: %s\n", pNumber)
 	http.ListenAndServe(pNumber, nil)
