2511ba1fabb577f9a5313ea31ec5787e5f162489
maziar <sojodyan@gmail.com>
2024-10-04 15:27:06 -0400

    add error handling for post

--- main.go
+++ main.go
@@ -20,9 +20,21 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Go API\n")
 }
 func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
 	var msg Message
-	body, _ := io.ReadAll(r.Body)
-	json.Unmarshal(body, &msg)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid read request body", http.StatusBadRequest)
+		return
+	}
+	err = json.Unmarshal(body, &msg)
+	if err != nil {
+		http.Error(w, "Invalid read request body", http.StatusBadRequest)
+		return
+	}
 	w.Header().Set("Content/Type", "application/json")
 	json.NewEncoder(w).Encode(msg)
 }
@@ -30,7 +42,6 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", getHandler)
 	http.HandleFunc("/post", postHandler)
-
 	pNumber := ":8091"
 	fmt.Printf("Server is running on the port: %s\n", pNumber)
 	http.ListenAndServe(pNumber, nil)
